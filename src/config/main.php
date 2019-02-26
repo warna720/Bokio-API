45c8f6bc36f885971c9c8ad2a211e7347943a237
Warhell Nasim <warhell1@hotmail.com>
2019-02-26 21:08:20 +0100

    Update main.php

--- src/config/main.php
+++ src/config/main.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 return [
     'email' => env('BOKIO_EMAIL'),
     'password' => env('BOKIO_PASSWORD'),
-    'debug' => env('BOKIO_DEBUG', true),
+    'debug' => env('BOKIO_DEBUG', false),
     'disable_sandbox' => env('BOKIO_DISABLE_SANDBOX',true),
     'disable_setuid_sandbox' => env('BOKIO_DISABLE_SETUID_SANDBOX', true),
 ];
